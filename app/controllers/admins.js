933b266364363945c701048d499bbab68ef66cdd
Vernon Govan <vernon.govan@gmail.com>
2015-09-21 05:28:02 -0500

    Add REST for admins

--- app/controllers/admins.js
+++ app/controllers/admins.js
@@ -2,26 +2,85 @@ var mongoose = require('mongoose');
 var Admin = mongoose.model('Admin');
 
 exports.index = function(req, res){
-  console.log("List admins");
-  res.send("List admins");
+  Admin.find(function(err, admins) {
+      if (err)
+          res.send(err);
+
+      res.json(admins);
+  });
 };
 
 exports.create = function(req, res){
-  console.log("Create admin");
-  res.send("Create admin");
+  var jsonString = '';
+
+  req.on('data', function (data) {
+    jsonString += data;
+  });
+
+  req.on('end', function () {
+    objJSON = JSON.parse(jsonString);
+
+    var admin = new Admin({
+      first_name: objJSON.first_name,
+      last_name: objJSON.last_name,
+      user_name: objJSON.user_name,
+      password: objJSON.password
+    });
+
+    admin.save(function (err) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Admin successfully created!' });
+    });
+  });
 };
 
 exports.show = function(req, res){
-  console.log("Get admin");
-  res.send("Get admin");
+  Admin.findById(req.params.id, function(err, admin) {
+      if (err)
+        res.send(err);
+
+      res.json(admin);
+  });
 };
 
 exports.update = function(req, res){
-  console.log("Update admin");
-  res.send("Update admin");
+  var jsonString = '';
+
+  req.on('data', function (data) {
+    jsonString += data;
+  });
+
+  req.on('end', function () {
+    objJSON = JSON.parse(jsonString);
+
+    var admin = {
+      first_name: objJSON.first_name,
+      last_name: objJSON.last_name,
+      user_name: objJSON.user_name,
+      password: objJSON.password
+    };
+
+    Admin.findByIdAndUpdate(req.params.id, admin, function(err, admin) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'Admin successfully updated!' });
+    });
+  });
 };
 
 exports.destroy = function(req, res){
-  console.log("Delete admin");
-  res.send("Delete admin");
+  Admin.findById(req.params.id, function(err, admin) {
+      if (err)
+          res.send(err);
+
+      admin.remove(function(err) {
+          if (err)
+              res.send(err);
+
+          res.json({ message: 'Admin successfully deleted!' });
+      });
+  });
 };
