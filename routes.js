647e0310e07e875c09e6067181af559105496bc6
Vernon Govan <vernon.govan@gmail.com>
2015-09-21 05:54:30 -0500

    Remove route prefix

--- routes.js
+++ routes.js
@@ -12,28 +12,28 @@ module.exports = function(app) {
 
   // user routes
   app.get('/users', userRoutes.index);
-  app.post('/users/create', userRoutes.create);
+  app.post('/users', userRoutes.create);
   app.get('/users/:id', userRoutes.show);
   app.put('/users/:id', userRoutes.update);
   app.delete('/users/:id', userRoutes.destroy);
 
   // admin routes
   app.get('/admins', adminRoutes.index);
-  app.post('/admins/create', adminRoutes.create);
+  app.post('/admins', adminRoutes.create);
   app.get('/admins/:id', adminRoutes.show);
   app.put('/admins/:id', adminRoutes.update);
   app.delete('/admins/:id', adminRoutes.destroy);
 
   // restaurant routes
   app.get('/restaurants', restaurantRoutes.index);
-  app.post('/restaurants/create', restaurantRoutes.create);
+  app.post('/restaurants', restaurantRoutes.create);
   app.get('/restaurants/:id', restaurantRoutes.show);
   app.put('/restaurants/:id', restaurantRoutes.update);
   app.delete('/restaurants/:id', restaurantRoutes.destroy);
 
   // rating routes
   app.get('/restaurants/:id/ratings', ratingRoutes.index);
-  app.post('/restaurants/:id/ratings/create', ratingRoutes.create);
+  app.post('/restaurants/:id/ratings', ratingRoutes.create);
   app.get('/restaurants/:id/ratings/:id', ratingRoutes.show);
   app.put('/restaurants/:id/ratings/:id', ratingRoutes.update);
   app.delete('/restaurants/:id/ratings/:id', ratingRoutes.destroy);
