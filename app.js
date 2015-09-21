aeb0a63d7488d2c8e88933386096682891259891
Vernon Govan <vernon.govan@gmail.com>
2015-09-21 05:17:05 -0500

    Add connection to MongoDB

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ var routes = require('./routes')(app);
 
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/foodiestest');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Mongoose connected');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
